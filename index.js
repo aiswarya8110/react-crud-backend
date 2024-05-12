45e86e2dc968c570d542a692acd953078b70fedf
aiswarya8110 <saish8110@gmail.com>
2024-05-12 23:34:46 +0530

    added condition for PORT

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 const moviesRouter = require("./routes/movies");
 const userRouter = require("./routes/user");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api', moviesRouter);
 app.use("/api/auth", userRouter);
